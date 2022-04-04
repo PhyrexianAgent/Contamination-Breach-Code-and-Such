067ed8e1d405e221b5096c2bd93c8215170170bb
Aidan <aidanlecl@gmail.com>
2022-04-04 16:57:41 -0400

    Added Ignore File and Fixed Ambiguity Issue

--- Contamination Breach/Assets/Scripts/PlayerCode.cs
+++ Contamination Breach/Assets/Scripts/PlayerCode.cs
@@ -5,9 +5,9 @@ using System;
 
 public class PlayerCode : MonoBehaviour
 {
-    public Rigidbody2D rigidbody;
-    public GameObject camera;
-    public float speed = 1000;
+    public Rigidbody2D rigidbodySelf;
+    public GameObject cameraFollowing;
+    public float speedSelf = 1000;
     void Start()
     {
         
@@ -16,23 +16,23 @@ public class PlayerCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-        camera.transform.position = new Vector3(transform.position.x, transform.position.y, -1); //keeps camra at player's position (if making that child of player, then camera will rotate with player)
+        ChangePlayerVelocity();
+        cameraFollowing.transform.position = new Vector3(transform.position.x, transform.position.y, -1); //keeps camra at player's position (if making that child of player, then camera will rotate with player)
     }
 
-    void MovePlayer() //uses input from player to generate normalized velocity (numbers always between 0 and 1). will then multiply that by speed and Time.deltaTime
+    void ChangePlayerVelocity() //uses input from player to generate normalized velocity (numbers always between 0 and 1). will then multiply that by speed and Time.deltaTime
     {
         bool[] inputTests = new bool[] { Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.S) };
         Vector2 velocityLocal = new Vector2(Convert.ToInt32(inputTests[1]) - Convert.ToInt32(inputTests[0]), Convert.ToInt32(inputTests[2]) - Convert.ToInt32(inputTests[3]));
         if (inputTests[0] || inputTests[1] || inputTests[2] || inputTests[3]) 
         {
-            rigidbody.velocity = velocityLocal.normalized * speed;
+            rigidbodySelf.velocity = velocityLocal.normalized * speedSelf;
             
             transform.rotation = Quaternion.identity;
         }
-        else if (rigidbody.velocity != new Vector2(0, 0)) //makes sure velocities from collisions don't cause any movement
+        else if (rigidbodySelf.velocity != new Vector2(0, 0)) //makes sure velocities from collisions don't cause any movement
         {
-            rigidbody.velocity = new Vector2(0, 0);
+            rigidbodySelf.velocity = new Vector2(0, 0);
         }
     }
 }
