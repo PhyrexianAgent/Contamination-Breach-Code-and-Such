e5cba53a69c83de55db70e60a1ba4dd6400939c7
Aidan <aidanlecl@gmail.com>
2022-04-12 16:46:24 -0400

    Added Player Basic Walking and Running Anims and Changed GitIgnore

--- Contamination Breach/Assets/Scripts/PlayerCode.cs
+++ Contamination Breach/Assets/Scripts/PlayerCode.cs
@@ -5,12 +5,20 @@ using System;
 
 public class PlayerCode : MonoBehaviour
 {
-    public Rigidbody2D rigidbody;
+    
     public GameObject camera;
     public GameObject flashlight;
+    public GameObject sprite;
     public float speed = 1000; //note this number is not correct since it is different in unity editor
+
+    private Rigidbody2D rigidbody;
+    private Animator animPlayer;
+    private bool isMoving = false;
+
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animPlayer = sprite.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +34,9 @@ public class PlayerCode : MonoBehaviour
         bool[] inputTests = new bool[] { Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.S) };
         Vector2 velocityLocal = new Vector2(Convert.ToInt32(inputTests[1]) - Convert.ToInt32(inputTests[0]), Convert.ToInt32(inputTests[2]) - Convert.ToInt32(inputTests[3]));
         rigidbody.velocity = velocityLocal.normalized * speed;
+        isMoving = velocityLocal != Vector2.zero;
+        animPlayer.SetBool("isIdle", !isMoving);
+        animPlayer.SetBool("isRunning", isMoving);
     }
 
         void RotateToMouse()
