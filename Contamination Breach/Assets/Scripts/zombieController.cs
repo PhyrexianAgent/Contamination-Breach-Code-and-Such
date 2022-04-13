5eb2165dbb510032ed5c09345e050574ec113e5a
Aidan <aidanlecl@gmail.com>
2022-04-13 19:59:31 -0400

    Navmesh Works and Is Included in this Commit
    Individual zombie is currently having this applied to but it is easy to make that for all of them.

--- Contamination Breach/Assets/Scripts/zombieController.cs
+++ Contamination Breach/Assets/Scripts/zombieController.cs
@@ -1,69 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class zombieController : MonoBehaviour
 {
+    public int pos;
+    public float speed;
+    public Transform target;
+
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator anim;
-    public int pos;
-    public float speed;
+    private NavMeshAgent agent;
+
+    
     
     void Start()
     {
        
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        //rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
 
-        walk();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.SetDestination(target.position);
     }
-    void walk()
-    {
-       
-        
-        
-            rb.velocity = new Vector2(0f, 1f * speed);
-            anim.SetBool("isWalking", true);
-       
-      
-       
-        
-    }
+    //void Walk()
+    //{
+    //    rb.velocity = new Vector2(0f, 1f * speed);
+    //    anim.SetBool("isWalking", true);
+    //}
 
-    public void WalkDown()
-    {
-
-        rb.velocity = new Vector2(0f, -1f * speed);
+    //public void WalkDown()
+    //{
+//
+ //       rb.velocity = new Vector2(0f, -1f * speed);
       
 
-    }
-    public void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "MR")
-        {
+//    }
+    //public void OnCollisionEnter2D(Collision2D other)
+    //{
+     //   if (other.gameObject.tag == "MR")
+     //   {
+//
+ //          
+  //          this.anim.SetBool("isWalking", false);
+   //         this.anim.SetBool("down", true);
+    //        this.WalkDown();
+     //   }
+      //  else if (other.gameObject.tag == "MU")
+       // {
+        //    this.anim.SetBool("down", false);
+         //   this.anim.SetBool("isWalking", true);
+          //  Walk();
 
-           
-            this.anim.SetBool("isWalking", false);
-            this.anim.SetBool("down", true);
-            this.WalkDown();
-        }
-        else if (other.gameObject.tag == "MU")
-        {
-            this.anim.SetBool("down", false);
-            this.anim.SetBool("isWalking", true);
-            walk();
-
-        }
-
-    }
+//        }
+//
+  //  }
 
 
 }
