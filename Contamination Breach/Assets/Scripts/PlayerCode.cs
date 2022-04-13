af5ceac5d6c0f5f32a6b73d982d2dd6ca6cd7e6a
Aidan <aidanlecl@gmail.com>
2022-04-13 18:27:09 -0400

    Added Animations for All Weapons

--- Contamination Breach/Assets/Scripts/PlayerCode.cs
+++ Contamination Breach/Assets/Scripts/PlayerCode.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PlayerCode : MonoBehaviour
+public class PlayerCode : MonoBehaviour //future note, the rifle reload is very slightly off but may not be noticable
 {
-    
+    const string STATE_IDLE = "Idle"; //for these constants, they reference parts of each animation name. each name should be WEAPON STATE to work with these
+    const string STATE_RUNNING = "Running";
+    const string STATE_ATTACK = "Attack";
+    const string STATE_RELOAD = "Reload";
+
+    const string WEAPON_KNIFE = "Knife";
+    const string WEAPON_HAND_GUN = "Hand Gun";
+    const string WEAPON_RIFLE = "Rifle";
+    const string WEAPON_SHOT_GUN = "Shot Gun";
+
     public GameObject camera;
     public GameObject flashlight;
     public GameObject sprite;
     public Animator animPlayer;
     public float speed = 1000; //note this number is not correct since it is different in unity editor
 
-    private enum States { idle, running, attacking }
-
     private Rigidbody2D rigidbody;
     
-
-    private int currentState = (int)States.idle;
+    private string currentState = STATE_IDLE;
+    private string currentWeapon = WEAPON_KNIFE;
+    private bool isAttacking = false;
+    private bool isReloading = false;
     
 
     void Start()
@@ -33,26 +42,28 @@ public class PlayerCode : MonoBehaviour
         camera.transform.position = new Vector3(transform.position.x, transform.position.y, -1); //keeps camra at player's position (if making that child of player, then camera will rotate with player)
         RotateToMouse();
         TestForInput();
-        ActionsFromState();
-    }
-
-    void ActionsFromState()
-    {
-        switch (currentState)
-        {
-            case (int)States.attacking:
-                if (!animPlayer.GetBool("attacking"))
-                    currentState = (int)States.idle;
-                break;
-        }
     }
 
     void TestForInput() //tests for various inputs to do various things not related to movement
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isReloading)
         {
-            currentState = (int)States.attacking;
-            animPlayer.SetTrigger("attack");
+            ChangeCurrentAnim(STATE_ATTACK);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (currentWeapon == WEAPON_KNIFE)
+            {
+                SwitchWeapon(WEAPON_SHOT_GUN);
+            }
+            else
+            {
+                SwitchWeapon(WEAPON_KNIFE);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ChangeCurrentAnim(STATE_RELOAD);
         }
     }
 
@@ -61,17 +72,70 @@ public class PlayerCode : MonoBehaviour
         bool[] inputTests = new bool[] { Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.S) };
         Vector2 velocityLocal = new Vector2(Convert.ToInt32(inputTests[1]) - Convert.ToInt32(inputTests[0]), Convert.ToInt32(inputTests[2]) - Convert.ToInt32(inputTests[3]));
         rigidbody.velocity = velocityLocal.normalized * speed;
-        //if (currentState == (int)States.attacking)
-          //  Debug.Log("entered here");
-        if (velocityLocal == Vector2.zero && currentState == (int)States.running)
+
+        if (!isAttacking && !isReloading)
         {
-            currentState = (int)States.idle;
+            if (velocityLocal == Vector2.zero)
+            {
+                ChangeCurrentAnim(STATE_IDLE);
+            }
+            else
+            {
+                ChangeCurrentAnim(STATE_RUNNING);
+            }
         }
-        else if (currentState == (int)States.idle && velocityLocal != Vector2.zero)
+        
+    }
+
+    void ChangeCurrentAnim(string newState)
+    {
+        string oldState = currentState;
+        if (currentState != newState)
         {
-            currentState = (int)States.running;
+            currentState = newState;
+            if (currentState != STATE_RELOAD || currentWeapon != WEAPON_KNIFE)
+            {
+                animPlayer.Play(currentWeapon + " " + currentState);
+            }
+            if (newState == STATE_ATTACK)
+            {
+                isAttacking = true;
+                Invoke("OnEndAttack", ChangeTimeFromWeapon(animPlayer.GetCurrentAnimatorStateInfo(0).length, oldState));
+            }
+            else if (newState == STATE_RELOAD && currentWeapon != WEAPON_KNIFE)
+            {
+                isReloading = true;
+                Debug.Log("doing anim");
+                Invoke("OnEndReload", ChangeTimeFromWeapon(animPlayer.GetCurrentAnimatorStateInfo(0).length, oldState));
+            }
         }
-        animPlayer.SetInteger("currentState", currentState);
+    }
+
+    float GetProperTime()
+    {
+        AnimatorStateInfo currentState = animPlayer.GetCurrentAnimatorStateInfo(0);
+        float lengthBase = currentState.length;
+        float playSpeed = currentState.speed;
+        return lengthBase - ((lengthBase * playSpeed) - lengthBase);
+    }
+
+    float ChangeTimeFromWeapon(float time, string oldState)
+    {
+        float subVal = 0;
+        switch (currentWeapon)
+        {
+            case WEAPON_KNIFE:
+                subVal = oldState == STATE_IDLE ? 1f : 0.6f;
+                break;
+            case WEAPON_HAND_GUN:
+                subVal = currentState != STATE_RELOAD ? 1.3f : 0.5f;
+                break;
+            case WEAPON_SHOT_GUN:
+            case WEAPON_RIFLE:
+                subVal = currentState == STATE_RELOAD ? 0.5f : 1.5f;
+                break;
+        }
+        return time - subVal;
     }
 
     void RotateToMouse()
@@ -80,10 +144,25 @@ public class PlayerCode : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
     }
 
-    public void OnAttackEnd()
+    void OnEndAttack()
     {
-        Debug.Log("got here");
-        currentState = (int)States.idle;
-        animPlayer.SetInteger("currentState", currentState);
+        isAttacking = false;
+        ChangeCurrentAnim(STATE_IDLE);
     }
+
+    void OnEndReload()
+    {
+        isReloading = false;
+        ChangeCurrentAnim(STATE_IDLE);
+        Debug.Log("getting to end");
+    }
+
+    void SwitchWeapon(string newWeapon)
+    {
+        currentWeapon = newWeapon;
+        animPlayer.Play(currentWeapon+" "+currentState);
+       
+    }
+
+    //.505 .53
 }
